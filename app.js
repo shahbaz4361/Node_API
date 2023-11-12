26693192c232bf2f23b1056f0b03c25b903d30ae
Shahbaz Jamal <135946699+shahbaz4361@users.noreply.github.com>
2023-11-13 00:05:26 +0530

    made changes

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import {connectDB} from "./config/database.js";
 import dotenv from "dotenv"
 dotenv.config()
 import productRouter from './routes/productRoutes.js';
+import errorMiddleware from "./middleware/error.js";
 
 const app = express();
 const port = process.env.PORT || 5000
@@ -14,13 +15,13 @@ connectDB()
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.json());
-
+app.use(errorMiddleware)
 app.get("/" , (req, res)=>{
     return res.send({message:"API is live"})
 })
 
 
-app.use('/api', productRouter)
+app.use('/api/product', productRouter)
 
 app.listen(port, (err) => {
     
