e1227d04b33fd361589aace8830b9425825b7243
Shahbaz Jamal <135946699+shahbaz4361@users.noreply.github.com>
2023-11-13 16:29:20 +0530

    changed

--- app.js
+++ app.js
@@ -14,9 +14,13 @@ const port = process.env.PORT || 5000
 connectDB()
 
 app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(cors())
+
 app.use(express.json());
+
 app.use(errorMiddleware)
+
 app.get("/" , (req, res)=>{
     return res.send({message:"API is live"})
 })
