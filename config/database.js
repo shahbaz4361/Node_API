4cafad6828a1a9abd6fe8c9ef52bb6cf2060dabf
Shahbaz Jamal <135946699+shahbaz4361@users.noreply.github.com>
2023-11-18 15:14:27 +0530

    Initial Commits

--- config/database.js
+++ config/database.js
@@ -1,7 +1,10 @@
 import mongoose from "mongoose"
 
 export const connectDB = ()=>{
- mongoose.connect(process.env.MONGODB_URI)
+ mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+ })
  .then(()=>{
     console.log("MongoDB Connected")
  })
